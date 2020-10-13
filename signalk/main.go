36ae1f861a6001ef80d16a329e478f1d7dac0dc3
munnik <martijndemunnik@protonmail.com>
2020-10-13 15:09:42 +0200

    Use easyjson feature to create lowerCamelCase json

--- signalk/main.go
+++ signalk/main.go
@@ -14,19 +14,19 @@ var (
 	}
 )
 
-// remember to run `easyjson -all signalk/main.go` when changing a struct
+// remember to run `easyjson -all -lower_camel_case signalk/main.go` when changing a struct
 
 // Delta as specified in https://signalk.org/specification/1.4.0/doc/data_model.html
 type Delta struct {
-	Context string   `json:"context,omitempty"`
-	Updates []Update `json:"updates"`
+	Context string `json:",omitempty"`
+	Updates []Update
 }
 
 // Full as specified in https://signalk.org/specification/1.4.0/doc/data_model.html
 type Full struct {
-	Version string                   `json:"version"`
-	Self    string                   `json:"self"`
-	Vessels map[string]*VesselValues `json:"vessels"`
+	Version string
+	Self    string
+	Vessels map[string]*VesselValues
 }
 
 // VesselValues element of a Full model
@@ -34,41 +34,41 @@ type VesselValues map[string]interface{}
 
 // Position is used for position values without altitude
 type Position struct {
-	Longitude float64 `json:"longitude"`
-	Latitude  float64 `json:"latitude"`
-	Altitude  float64 `json:"altitude,omitempty"`
+	Longitude float64
+	Latitude  float64
+	Altitude  float64 `json:",omitempty"`
 }
 
 // Length is used for the combination of all lengths
 type Length struct {
-	Overall   float64 `json:"overall,omitempty"`
-	Hull      float64 `json:"hull,omitempty"`
-	Waterline float64 `json:"waterline,omitempty"`
+	Overall   float64 `json:",omitempty"`
+	Hull      float64 `json:",omitempty"`
+	Waterline float64 `json:",omitempty"`
 }
 
 // Value is part of an Update
 type Value struct {
-	Context   string      `json:"context,omitempty"`
-	Path      []string    `json:"path,omitempty"`
-	Value     interface{} `json:"value"`
-	Source    Source      `json:"source"`
-	Timestamp time.Time   `json:"timestamp"`
+	Context   string   `json:",omitempty"`
+	Path      []string `json:",omitempty"`
+	Value     interface{}
+	Source    Source
+	Timestamp time.Time
 }
 
 // Source is part of an Update
 type Source struct {
-	Label    string `json:"label"`
-	Type     string `json:"type"`
-	Talker   string `json:"talker,omitempty"`
-	Sentence string `json:"sentence,omitempty"`
-	AisType  uint8  `json:"aisType,omitempty"`
+	Label    string
+	Type     string
+	Talker   string `json:",omitempty"`
+	Sentence string `json:",omitempty"`
+	AisType  uint8  `json:",omitempty"`
 }
 
 // Update is part of a Delta
 type Update struct {
-	Source    Source  `json:"source"`
-	Timestamp string  `json:"timestamp"`
-	Values    []Value `json:"values"`
+	Source    Source
+	Timestamp string
+	Values    []Value
 }
 
 // NewFull creates a Full model

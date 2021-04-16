a4d85f04237fe89ba7affc951a96e6f64c124d32
munnik <martijndemunnik@protonmail.com>
2021-04-16 10:10:55 +0200

    Extracted nmea to separate module

--- go.mod
+++ go.mod
@@ -3,23 +3,20 @@ module github.com/munnik/gosk
 go 1.16
 
 require (
-	github.com/BertoldVdb/go-ais v0.0.0-20210129141056-62b4c9149def
 	github.com/Microsoft/go-winio v0.4.17 // indirect
-	github.com/adrianmo/go-nmea v1.3.0
 	github.com/antonmedv/expr v1.8.9
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/goccy/go-json v0.4.11
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/websocket v1.4.2
 	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
 	github.com/jackc/pgx/v4 v4.11.0
 	github.com/magiconair/properties v1.8.5 // indirect
-	github.com/mailru/easyjson v0.7.7
 	github.com/martinlindhe/unit v0.0.0-20210313160520-19b60e03648d
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
-	github.com/onsi/ginkgo v1.16.1 // indirect
-	github.com/onsi/gomega v1.11.0 // indirect
+	github.com/munnik/nmea0183 v0.0.0-20210416080533-fe846cb912fd // indirect
+	github.com/onsi/ginkgo v1.16.1
+	github.com/onsi/gomega v1.11.0
 	github.com/pelletier/go-toml v1.9.0 // indirect
 	github.com/simonvetter/modbus v1.3.0
 	github.com/spf13/afero v1.6.0 // indirect
@@ -27,7 +24,7 @@ require (
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/viper v1.7.1
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
 	go.nanomsg.org/mangos/v3 v3.2.1
 	go.uber.org/multierr v1.6.0 // indirect
@@ -35,7 +32,6 @@ require (
 	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
 	golang.org/x/sys v0.0.0-20210414055047-fe65e336abe0 // indirect
 	golang.org/x/tools v0.1.0 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.26.0
 	gopkg.in/ini.v1 v1.62.0 // indirect
 )
--- mapper/nmea/keyvalue.go
+++ mapper/nmea/keyvalue.go
@@ -6,155 +6,156 @@ import (
 	"github.com/munnik/gosk/config"
 	"github.com/munnik/gosk/mapper/signalk"
 	"github.com/munnik/gosk/nanomsg"
+	"github.com/munnik/nmea0183"
 )
 
 // KeyValueFromNMEA0183 tries to create a Signal K Delta from a NMEA sentence
 func KeyValueFromNMEA0183(m *nanomsg.RawData, cfg config.NMEA0183Config) ([]signalk.Value, error) {
 	result := make([]signalk.Value, 0)
-	sentence, err := Parse(string(m.Payload))
+	sentence, err := nmea0183.Parse(string(m.Payload))
 	if err != nil {
 		return result, err
 	}
 
 	context := cfg.Context
-	if v, ok := sentence.(MMSI); ok {
+	if v, ok := sentence.(nmea0183.MMSI); ok {
 		if mmsi, err := v.GetMMSI(); err == nil {
 			context = fmt.Sprintf("vessels.urn:mrn:imo:mmsi:%d", mmsi)
 			result = append(result, signalk.Value{Context: context, Path: []string{""}, Value: &nanomsg.VesselDataValue{Mmsi: &mmsi}})
 		}
 	}
 
-	if v, ok := sentence.(DepthBelowSurface); ok {
+	if v, ok := sentence.(nmea0183.DepthBelowSurface); ok {
 		if depthBelowSurface, err := v.GetDepthBelowSurface(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"environment", "depth", "belowSurface"}, Value: nanomsg.Double(depthBelowSurface)})
 		}
 	}
-	if v, ok := sentence.(DepthBelowTransducer); ok {
+	if v, ok := sentence.(nmea0183.DepthBelowTransducer); ok {
 		if depthBelowTransducer, err := v.GetDepthBelowTransducer(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"environment", "depth", "belowTransducer"}, Value: nanomsg.Double(depthBelowTransducer)})
 		}
 	}
-	if v, ok := sentence.(FixQuality); ok {
+	if v, ok := sentence.(nmea0183.FixQuality); ok {
 		if fixQuality, err := v.GetFixQuality(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"navigation", "gnss", "methodQuality"}, Value: nanomsg.String(fixQuality)})
 		}
 	}
-	if v, ok := sentence.(FixType); ok {
+	if v, ok := sentence.(nmea0183.FixType); ok {
 		if fixType, err := v.GetFixType(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"navigation", "gnss", "type"}, Value: nanomsg.String(fixType)})
 		}
 	}
-	if v, ok := sentence.(RateOfTurn); ok {
+	if v, ok := sentence.(nmea0183.RateOfTurn); ok {
 		if rateOfTurn, err := v.GetRateOfTurn(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"navigation", "rateOfTurn"}, Value: nanomsg.Double(rateOfTurn)})
 		}
 	}
-	if v, ok := sentence.(TrueCourseOverGround); ok {
+	if v, ok := sentence.(nmea0183.TrueCourseOverGround); ok {
 		if trueCourseOverGround, err := v.GetTrueCourseOverGround(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"navigation", "courseOverGroundTrue"}, Value: nanomsg.Double(trueCourseOverGround)})
 		}
 	}
-	if v, ok := sentence.(TrueHeading); ok {
+	if v, ok := sentence.(nmea0183.TrueHeading); ok {
 		if trueHeading, err := v.GetTrueHeading(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"navigation", "headingTrue"}, Value: nanomsg.Double(trueHeading)})
 		}
 	}
-	if v, ok := sentence.(MagneticHeading); ok {
+	if v, ok := sentence.(nmea0183.MagneticHeading); ok {
 		if magneticHeading, err := v.GetMagneticHeading(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"navigation", "headingMagnetic"}, Value: nanomsg.Double(magneticHeading)})
 		}
 	}
-	if v, ok := sentence.(NavigationStatus); ok {
+	if v, ok := sentence.(nmea0183.NavigationStatus); ok {
 		if navigationStatus, err := v.GetNavigationStatus(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"navigation", "state"}, Value: nanomsg.String(navigationStatus)})
 		}
 	}
-	if v, ok := sentence.(NumberOfSatelites); ok {
+	if v, ok := sentence.(nmea0183.NumberOfSatelites); ok {
 		if numberOfSatelites, err := v.GetNumberOfSatelites(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"navigation", "gnss", "satellites"}, Value: nanomsg.Int64(numberOfSatelites)})
 		}
 	}
-	if v, ok := sentence.(Position2D); ok {
+	if v, ok := sentence.(nmea0183.Position2D); ok {
 		if lat, lon, err := v.GetPosition2D(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"navigation", "position"}, Value: &nanomsg.PositionValue{Longitude: &lon, Latitude: &lat}})
 		}
 	}
-	if v, ok := sentence.(Position3D); ok {
+	if v, ok := sentence.(nmea0183.Position3D); ok {
 		if lat, lon, alt, err := v.GetPosition3D(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"navigation", "position"}, Value: &nanomsg.PositionValue{Longitude: &lon, Latitude: &lat, Altitude: &alt}})
 		}
 	}
-	if v, ok := sentence.(SpeedOverGround); ok {
+	if v, ok := sentence.(nmea0183.SpeedOverGround); ok {
 		if speedOverGround, err := v.GetSpeedOverGround(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"navigation", "speedOverGround"}, Value: nanomsg.Double(speedOverGround)})
 		}
 	}
-	if v, ok := sentence.(SpeedThroughWater); ok {
+	if v, ok := sentence.(nmea0183.SpeedThroughWater); ok {
 		if speedThroughWater, err := v.GetSpeedThroughWater(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"navigation", "speedThroughWater"}, Value: nanomsg.Double(speedThroughWater)})
 		}
 	}
-	if v, ok := sentence.(VesselName); ok {
+	if v, ok := sentence.(nmea0183.VesselName); ok {
 		if vesselName, err := v.GetVesselName(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{""}, Value: &nanomsg.VesselDataValue{Name: &vesselName}})
 		}
 	}
-	if v, ok := sentence.(CallSign); ok {
+	if v, ok := sentence.(nmea0183.CallSign); ok {
 		if callSign, err := v.GetCallSign(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"communication", "callsignVhf"}, Value: nanomsg.String(callSign)})
 		}
 	}
-	if v, ok := sentence.(IMONumber); ok {
+	if v, ok := sentence.(nmea0183.IMONumber); ok {
 		if imoNumber, err := v.GetIMONumber(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"registrations", "imo"}, Value: nanomsg.String(imoNumber)})
 		}
 	}
-	if v, ok := sentence.(ENINumber); ok {
+	if v, ok := sentence.(nmea0183.ENINumber); ok {
 		if eniNumber, err := v.GetENINumber(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"registrations", "other", "eni", "registration"}, Value: nanomsg.String(eniNumber)})
 		}
 	}
-	if v, ok := sentence.(VesselLength); ok {
+	if v, ok := sentence.(nmea0183.VesselLength); ok {
 		if length, err := v.GetVesselLength(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"design", "length"}, Value: &nanomsg.LengthValue{Overall: &length}})
 		}
 	}
-	if v, ok := sentence.(VesselBeam); ok {
+	if v, ok := sentence.(nmea0183.VesselBeam); ok {
 		if beam, err := v.GetVesselBeam(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"design", "beam"}, Value: nanomsg.Double(beam)})
 		}
 	}
-	if v, ok := sentence.(TrueWindDirection); ok {
+	if v, ok := sentence.(nmea0183.TrueWindDirection); ok {
 		if trueWindDirection, err := v.GetTrueWindDirection(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"environment", "wind", "directionTrue"}, Value: nanomsg.Double(trueWindDirection)})
 		}
 	}
-	if v, ok := sentence.(MagneticWindDirection); ok {
+	if v, ok := sentence.(nmea0183.MagneticWindDirection); ok {
 		if magneticWindDirection, err := v.GetMagneticWindDirection(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"environment", "wind", "directionMagnetic"}, Value: nanomsg.Double(magneticWindDirection)})
 		}
 	}
-	if v, ok := sentence.(WindSpeed); ok {
+	if v, ok := sentence.(nmea0183.WindSpeed); ok {
 		if windSpeed, err := v.GetWindSpeed(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"environment", "wind", "speedOverGround"}, Value: nanomsg.Double(windSpeed)})
 		}
 	}
-	if v, ok := sentence.(OutsideTemperature); ok {
+	if v, ok := sentence.(nmea0183.OutsideTemperature); ok {
 		if outsideTemperature, err := v.GetOutsideTemperature(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"environment", "outside", "temperature"}, Value: nanomsg.Double(outsideTemperature)})
 		}
 	}
-	if v, ok := sentence.(DewPointTemperature); ok {
+	if v, ok := sentence.(nmea0183.DewPointTemperature); ok {
 		if dewPointTemperature, err := v.GetDewPointTemperature(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"environment", "outside", "dewPointTemperature"}, Value: nanomsg.Double(dewPointTemperature)})
 		}
 	}
-	if v, ok := sentence.(Humidity); ok {
+	if v, ok := sentence.(nmea0183.Humidity); ok {
 		if humidity, err := v.GetHumidity(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"environment", "outside", "humidity"}, Value: nanomsg.Double(humidity)})
 		}
 	}
-	if v, ok := sentence.(WaterTemperature); ok {
+	if v, ok := sentence.(nmea0183.WaterTemperature); ok {
 		if waterTemperature, err := v.GetWaterTemperature(); err == nil {
 			result = append(result, signalk.Value{Context: context, Path: []string{"environment", "water", "temperature"}, Value: nanomsg.Double(waterTemperature)})
 		}

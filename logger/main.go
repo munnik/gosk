aea694c2f2cd34d7197ccbf962d5d73bd4b40c86
munnik <martijndemunnik@protonmail.com>
2022-02-20 16:06:05 +0100

    Log to stderr

--- logger/main.go
+++ logger/main.go
@@ -1,11 +1,7 @@
 package logger
 
 import (
-	"log/syslog"
-
-	"github.com/tchap/zapext/v2/zapsyslog"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 var logger *zap.Logger
@@ -13,10 +9,7 @@ var logger *zap.Logger
 // GetLogger returns the logger of the application
 func GetLogger() *zap.Logger {
 	if logger == nil {
-		writer, _ := syslog.New(syslog.LOG_ERR|syslog.LOG_LOCAL0, "")
-		encoder := zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
-		core := zapsyslog.NewCore(zapcore.InfoLevel, encoder, writer)
-		logger = zap.New(core)
+		logger, _ = zap.NewProduction()
 	}
 	return logger
 }

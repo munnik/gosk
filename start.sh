b7520bab73c2b5609e7e5c5871bb0ecce2b7bf26
munnik <martijndemunnik@protonmail.com>
2022-03-13 10:16:29 +0100

    Wait only 1 second before starting next services

--- start.sh
+++ start.sh
@@ -6,17 +6,17 @@ pkill gosk
 ./gosk collect -p "tcp://127.0.0.1:6001" --config "config/collector/c32.yaml" &
 ./gosk collect -p "tcp://127.0.0.1:6002" --config "config/collector/zmg.yaml" &
 
-sleep 5
+sleep 1
 
 ./gosk map -p "tcp://127.0.0.1:6011" -s "tcp://127.0.0.1:6001" --config "config/mapper/c32.yaml" &
 ./gosk map -p "tcp://127.0.0.1:6012" -s "tcp://127.0.0.1:6002" --config "config/mapper/ais.yaml" &
 
-sleep 5
+sleep 1
 
 ./gosk proxy -p "tcp://127.0.0.1:6000" -s "tcp://127.0.0.1:6001" -s "tcp://127.0.0.1:6002" &
 ./gosk proxy -p "tcp://127.0.0.1:6010" -s "tcp://127.0.0.1:6011" -s "tcp://127.0.0.1:6012" &
 
-sleep 5
+sleep 1
 
 ./gosk write database raw -s "tcp://127.0.0.1:6000"  --config "config/writer/postgresql.yaml" &
 ./gosk write database mapped -s "tcp://127.0.0.1:6010"  --config "config/writer/postgresql.yaml" &

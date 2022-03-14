f9aa25e1922741fe03330dfec4ca88d9896e1854
munnik <martijndemunnik@protonmail.com>
2022-03-14 18:54:53 +0100

    Use empty path for mmsi and name

---
+++ database/migrations/20220314160554_empty_path_vessel_info.down.sql
@@ -0,0 +1,29 @@
+UPDATE "mapped_data" SET "value" = "value"->'name', "path" = 'name' WHERE "path" = '' AND "value" ? 'name';
+UPDATE "mapped_data" SET "value" = "value"->'mmsi', "path" = 'mmsi' WHERE "path" = '' AND "value" ? 'mmsi';
+
+CREATE OR REPLACE FUNCTION "update_name"() RETURNS TRIGGER AS $$ BEGIN
+INSERT INTO "static_data" ("context", "name")
+VALUES (NEW."context", NEW."value") ON CONFLICT("context") DO
+UPDATE
+SET "name" = NEW."value";
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+DROP TRIGGER "update_name_trigger" ON "mapped_data";
+CREATE TRIGGER "update_name_trigger"
+AFTER
+INSERT ON "mapped_data" FOR EACH ROW
+    WHEN (NEW."path" = 'name') EXECUTE PROCEDURE "update_name"();
+CREATE OR REPLACE FUNCTION "update_mmsi"() RETURNS TRIGGER AS $$ BEGIN
+INSERT INTO "static_data" ("context", "mmsi")
+VALUES (NEW."context", NEW."value") ON CONFLICT("context") DO
+UPDATE
+SET "mmsi" = NEW."value";
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+DROP TRIGGER "update_mmsi_trigger" ON "mapped_data";
+CREATE TRIGGER "update_mmsi_trigger"
+AFTER
+INSERT ON "mapped_data" FOR EACH ROW
+    WHEN (NEW."path" = 'mmsi') EXECUTE PROCEDURE "update_mmsi"();
---
+++ database/migrations/20220314160554_empty_path_vessel_info.up.sql
@@ -0,0 +1,29 @@
+UPDATE "mapped_data" SET "value" = replace('{"name": #}', '#', "value"::TEXT)::JSONB, "path" = '' WHERE "path" = 'name';
+UPDATE "mapped_data" SET "value" = replace('{"mmsi": #}', '#', "value"::TEXT)::JSONB, "path" = '' WHERE "path" = 'mmsi';
+
+CREATE OR REPLACE FUNCTION "update_name"() RETURNS TRIGGER AS $$ BEGIN
+INSERT INTO "static_data" ("context", "name")
+VALUES (NEW."context", NEW."value"->>'name') ON CONFLICT("context") DO
+UPDATE
+SET "name" = NEW."value"->>'name';
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+DROP TRIGGER "update_name_trigger" ON "mapped_data";
+CREATE TRIGGER "update_name_trigger"
+AFTER
+INSERT ON "mapped_data" FOR EACH ROW
+    WHEN (NEW."path" = '' AND NEW."value" ? 'name') EXECUTE PROCEDURE "update_name"();
+CREATE OR REPLACE FUNCTION "update_mmsi"() RETURNS TRIGGER AS $$ BEGIN
+INSERT INTO "static_data" ("context", "mmsi")
+VALUES (NEW."context", NEW."value"->>'mmsi') ON CONFLICT("context") DO
+UPDATE
+SET "mmsi" = NEW."value"->>'mmsi';
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+DROP TRIGGER "update_mmsi_trigger" ON "mapped_data";
+CREATE TRIGGER "update_mmsi_trigger"
+AFTER
+INSERT ON "mapped_data" FOR EACH ROW
+    WHEN (NEW."path" = '' AND NEW."value" ? 'mmsi') EXECUTE PROCEDURE "update_mmsi"();

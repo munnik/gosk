df4d3f585085d1d8819b7ed42e3aa85f3303290a
munnik <martijndemunnik@protonmail.com>
2022-07-29 13:06:53 +0200

    Create and drop table

---
+++ database/migrations/20220729101612_remote_data.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "remote_data" (
+    "origin" TEXT NOT NULL DEFAULT 'unknown', 
+    "start" TIMESTAMP WITH TIME ZONE NOT NULL, 
+    "end" TIMESTAMP WITH TIME ZONE NOT NULL, 
+    "local" INTEGER NOT NULL DEFAULT -1, 
+    "remote" INTEGER NOT NULL DEFAULT 0,
+);
+SELECT create_hypertable('remote_data', 'start');

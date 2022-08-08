50f49b79f09dafccb42d06ce7b3cac1ac3aa4547
Albert Smit <as@fuelessence.com>
2022-08-08 10:25:32 +0200

    added correct primary key to table

--- database/migrations/20220729101612_remote_data.up.sql
+++ database/migrations/20220729101612_remote_data.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE "remote_data" (
-    "origin" TEXT PRIMARY KEY, 
+    "origin" TEXT NOT NULL, 
     "start" TIMESTAMP WITH TIME ZONE NOT NULL, 
     "end" TIMESTAMP WITH TIME ZONE NOT NULL, 
     "local" INTEGER NOT NULL DEFAULT -1, 
-    "remote" INTEGER NOT NULL DEFAULT 0
+    "remote" INTEGER NOT NULL DEFAULT 0,
+    PRIMARY KEY ("origin","start"))
 );

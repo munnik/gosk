cffe3bcee3819e12835004b373f9211daf8132bc
munnik <martijndemunnik@protonmail.com>
2022-08-30 22:19:36 +0200

    Simplified code

--- database/postgresql_buffer.go
+++ database/postgresql_buffer.go
@@ -30,35 +30,43 @@ func NewPostgresqlBuffer(pool *pgxpool.Pool, table string, columnNames []string,
 		rows:        make([][]interface{}, 0, size+1),
 		columnNames: columnNames,
 	}
-	go func() {
-		ticker := time.NewTicker(time.Second * time.Duration(flushInterval))
-		for {
-			<-ticker.C
-			if time.Now().Add(-time.Second * time.Duration(flushInterval)).Before(result.lastFlush) {
-				result.flush()
-			}
-		}
-	}()
+	go result.startTicker(flushInterval)
 
 	return result
 }
 
 func (b *PostgresqlBuffer) Add(row []interface{}) {
 	b.mu.Lock()
+	defer b.mu.Unlock()
 	b.rows = append(b.rows, row)
-	l := len(b.rows)
-	b.mu.Unlock()
 
-	if l >= b.size {
+	if len(b.rows) >= b.size {
 		b.flush()
 	}
 }
 
-func (b *PostgresqlBuffer) flush() {
-	b.mu.Lock()
-	defer b.mu.Unlock()
+func (b *PostgresqlBuffer) startTicker(flushInterval int) {
+	ticker := time.NewTicker(time.Second * time.Duration(flushInterval))
+	for {
+		<-ticker.C
+		b.mu.Lock()
+		if time.Now().Add(-time.Second * time.Duration(flushInterval)).Before(b.lastFlush) {
+			b.flush()
+		}
+		defer b.mu.Unlock()
+	}
+}
 
-	src := pgx.CopyFromRows(b.rows)
+func (b *PostgresqlBuffer) flush() {
+	c := make([][]interface{}, len(b.rows))
+	copy(c, b.rows)
+	go b.copyFrom(pgx.CopyFromRows(c))
+	b.rows = make([][]interface{}, 0, b.size+1)
+	b.lastFlush = time.Now()
+
+}
+
+func (b *PostgresqlBuffer) copyFrom(src pgx.CopyFromSource) {
 	_, err := b.pool.CopyFrom(context.Background(), b.table, b.columnNames, src)
 	if err != nil {
 		logger.GetLogger().Error(
@@ -72,6 +80,4 @@ func (b *PostgresqlBuffer) flush() {
 			zap.String("Error", src.Err().Error()),
 		)
 	}
-	b.rows = make([][]interface{}, 0, b.size+1)
-	b.lastFlush = time.Now()
 }

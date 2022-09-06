cd5d49aa4d53eb3f7f78902875e8fdfe72bceaa9
Albert Smit <as@fuelessence.com>
2022-09-06 11:07:07 +0200

    drop triggers to stop database deadlocks

---
+++ database/migrations/20220906084811_drop_triggers.down.sql
@@ -0,0 +1,34 @@
+CREATE TRIGGER "update_name_trigger"
+AFTER
+INSERT ON "mapped_data" FOR EACH ROW
+    WHEN (NEW."path" = '' AND NEW."value" ? 'name') EXECUTE PROCEDURE "update_name"();
+
+CREATE TRIGGER "update_mmsi_trigger"
+AFTER
+INSERT ON "mapped_data" FOR EACH ROW
+    WHEN (NEW."path" = '' AND NEW."value" ? 'mmsi') EXECUTE PROCEDURE "update_mmsi"();
+
+CREATE TRIGGER "update_callsignvhf_trigger"
+AFTER
+INSERT ON "mapped_data" FOR EACH ROW
+    WHEN (NEW."path" = 'communication.callsignVhf') EXECUTE PROCEDURE "update_callsignvhf"();
+
+CREATE TRIGGER "update_eninumber_trigger"
+AFTER
+INSERT ON "mapped_data" FOR EACH ROW
+    WHEN (NEW."path" = 'registrations.other.eni.registration') EXECUTE PROCEDURE "update_eninumber"();
+
+CREATE TRIGGER "update_length_trigger"
+AFTER
+INSERT ON "mapped_data" FOR EACH ROW
+    WHEN (NEW."path" = 'design.length' AND NEW."value" ? 'overall') EXECUTE PROCEDURE "update_length"();
+
+CREATE TRIGGER "update_beam_trigger"
+AFTER
+INSERT ON "mapped_data" FOR EACH ROW
+    WHEN (NEW."path" = 'design.beam') EXECUTE PROCEDURE "update_beam"();
+
+CREATE TRIGGER "update_vesseltype_trigger"
+AFTER
+INSERT ON "mapped_data" FOR EACH ROW
+    WHEN (NEW."path" = 'design.aisShipType' AND NEW."value" ? 'name') EXECUTE PROCEDURE "update_vesseltype"();
---
+++ database/migrations/20220906084811_drop_triggers.up.sql
@@ -0,0 +1,7 @@
+DROP TRIGGER update_beam_trigger ON mapped_data;
+DROP TRIGGER update_callsignvhf_trigger ON mapped_data;
+DROP TRIGGER update_eninumber_trigger ON mapped_data;
+DROP TRIGGER update_length_trigger ON mapped_data;
+DROP TRIGGER update_mmsi_trigger ON mapped_data;
+DROP TRIGGER update_name_trigger ON mapped_data;
+DROP TRIGGER update_vesseltype_trigger ON mapped_data;

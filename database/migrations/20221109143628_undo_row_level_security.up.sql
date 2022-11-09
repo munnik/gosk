852cf7dc292719becde195db9685b662fe848401
munnik <martijndemunnik@protonmail.com>
2022-11-09 15:37:33 +0100

    Undo row level security

---
+++ database/migrations/20221109143628_undo_row_level_security.up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE mapped_data DISABLE ROW LEVEL SECURITY;
+DROP POLICY read_mapped_data on mapped_data;
+ALTER TABLE organization_vessels DISABLE ROW LEVEL SECURITY;
+DROP POLICY read_vessels on organization_vessels;
+DROP TABLE organization_vessels;

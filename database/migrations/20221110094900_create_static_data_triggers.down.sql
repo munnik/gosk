ea792480485cfa3b4c95ab2329de68667e69f9a8
munnik <martijndemunnik@protonmail.com>
2022-11-22 02:58:54 +0100

    Forgot to create functions

--- database/migrations/20221110094900_create_static_data_triggers.down.sql
+++ database/migrations/20221110094900_create_static_data_triggers.down.sql
@@ -5,3 +5,11 @@ DROP TRIGGER update_length_trigger ON mapped_data;
 DROP TRIGGER update_mmsi_trigger ON mapped_data;
 DROP TRIGGER update_name_trigger ON mapped_data;
 DROP TRIGGER update_vesseltype_trigger ON mapped_data;
+
+DROP FUNCTION "update_beam"();
+DROP FUNCTION "update_callsignvhf"();
+DROP FUNCTION "update_eninumber"();
+DROP FUNCTION "update_length"();
+DROP FUNCTION "update_mmsi"();
+DROP FUNCTION "update_name"();
+DROP FUNCTION "update_vesseltype"();
--- database/migrations/20221110094900_create_static_data_triggers.up.sql
+++ database/migrations/20221110094900_create_static_data_triggers.up.sql
@@ -1,3 +1,66 @@
+CREATE OR REPLACE FUNCTION "update_name"() RETURNS TRIGGER AS $$ BEGIN
+INSERT INTO "static_data" ("context", "name")
+VALUES (NEW."context", NEW."value"->>'name') ON CONFLICT("context") DO
+UPDATE
+SET "name" = NEW."value"->>'name';
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION "update_mmsi"() RETURNS TRIGGER AS $$ BEGIN
+INSERT INTO "static_data" ("context", "mmsi")
+VALUES (NEW."context", NEW."value"->>'mmsi') ON CONFLICT("context") DO
+UPDATE
+SET "mmsi" = NEW."value"->>'mmsi';
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION "update_callsignvhf"() RETURNS TRIGGER AS $$ BEGIN
+INSERT INTO "static_data" ("context", "callsignvhf")
+VALUES (NEW."context", trim(both '"' FROM NEW."value"::TEXT)) ON CONFLICT("context") DO
+UPDATE
+SET "callsignvhf" = trim(both '"' FROM NEW."value"::TEXT);
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION "update_eninumber"() RETURNS TRIGGER AS $$ BEGIN
+INSERT INTO "static_data" ("context", "eninumber")
+VALUES (NEW."context", trim(both '"' FROM NEW."value"::TEXT)) ON CONFLICT("context") DO
+UPDATE
+SET "eninumber" = trim(both '"' FROM NEW."value"::TEXT);
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION "update_length"() RETURNS TRIGGER AS $$ BEGIN
+INSERT INTO "static_data" ("context", "length")
+VALUES (NEW."context", (NEW."value"->'overall')::DOUBLE PRECISION) ON CONFLICT("context") DO
+UPDATE
+SET "length" = (NEW."value"->'overall')::DOUBLE PRECISION;
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION "update_beam"() RETURNS TRIGGER AS $$ BEGIN
+INSERT INTO "static_data" ("context", "beam")
+VALUES (NEW."context", NEW."value"::DOUBLE PRECISION) ON CONFLICT("context") DO
+UPDATE
+SET "beam" = NEW."value"::DOUBLE PRECISION;
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION "update_vesseltype"() RETURNS TRIGGER AS $$ BEGIN
+INSERT INTO "static_data" ("context", "vesseltype")
+VALUES (NEW."context", NEW."value"->>'name') ON CONFLICT("context") DO
+UPDATE
+SET "vesseltype" = NEW."value"->>'name';
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
 CREATE TRIGGER "update_name_trigger"
 AFTER
 INSERT ON "mapped_data" FOR EACH ROW

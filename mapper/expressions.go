819cc9571d2b1297836dc3b263f187a3a76acb0c
munnik <martijndemunnik@protonmail.com>
2022-11-24 19:35:44 +0100

    Tests for expressions

--- mapper/expressions.go
+++ mapper/expressions.go
@@ -6,16 +6,16 @@ type ExpressionEnvironment map[string]interface{}
 
 func NewExpressionEnvironment() ExpressionEnvironment {
 	return ExpressionEnvironment{
-		"currentToRatio":   currentToRatio,
-		"pressureToHeight": pressureToHeight,
-		"heightToVolume":   heightToVolume,
+		"currentToRatio":   CurrentToRatio,
+		"pressureToHeight": PressureToHeight,
+		"heightToVolume":   HeightToVolume,
 	}
 }
 
 // Returns the 4-20mA input signal to a ratio, 4000uA => 0.0, 8000uA => 0.25, 12000uA => 0.5, 16000uA => 0.75, 20000uA => 1.0
 // current is in uA (1000000uA is 1A)
 // return value is a ratio (0.0 .. 1.0)
-func currentToRatio(current float64) float64 {
+func CurrentToRatio(current float64) float64 {
 	return (current - 4000) / 16000
 }
 
@@ -23,87 +23,30 @@ func currentToRatio(current float64) float64 {
 // pressure is in Pa (1 Bar is 100000 Pascal)
 // density is in kg/m3 (typical value for diesel is 840)
 // return value is in m
-func pressureToHeight(pressure float64, density float64) float64 {
+func PressureToHeight(pressure float64, density float64) float64 {
 	G := 9.8 // acceleration due to gravity
 	return pressure / (density * G)
 }
 
-// Returns the heightToVolume corresponding to the measured height. This function is used when a pressure sensor is used in a tank.
+// Returns the HeightToVolume corresponding to the measured height. This function is used when a pressure sensor is used in a tank.
 // height is in m
 // sensorOffset is in m (positive means that the sensor is placed above the bottom of the tank, negative value means that the sensor is place below the tank)
 // heights is in m, list of heights with corresponding volumes
 // volumes is in m3, list of volumes with corresponding heights
 // return value is in m3
-func heightToVolume(height float64, sensorOffset float64, heights []interface{}, volumes []interface{}) (result float64, err error) {
+func HeightToVolume(height float64, sensorOffset float64, heights []interface{}, volumes []interface{}) (result float64, err error) {
 	if len(heights) != len(volumes) {
 		err = fmt.Errorf("The list of heights should have the same length as the list of volumes, the lengths are %d and %d", len(heights), len(volumes))
 		return
 	}
 
-	heightFloats := make([]float64, len(heights))
-	volumeFloats := make([]float64, len(volumes))
-
-	for i, h := range heights {
-		switch t := h.(type) {
-		case int:
-			heightFloats[i] = float64(t)
-		case uint:
-			heightFloats[i] = float64(t)
-		case int8:
-			heightFloats[i] = float64(t)
-		case uint8:
-			heightFloats[i] = float64(t)
-		case int16:
-			heightFloats[i] = float64(t)
-		case uint16:
-			heightFloats[i] = float64(t)
-		case int32:
-			heightFloats[i] = float64(t)
-		case uint32:
-			heightFloats[i] = float64(t)
-		case int64:
-			heightFloats[i] = float64(t)
-		case uint64:
-			heightFloats[i] = float64(t)
-		case float32:
-			heightFloats[i] = float64(t)
-		case float64:
-			heightFloats[i] = t
-		default:
-			err = fmt.Errorf("The height in position %d of the heights can not be converted to a float64", i)
-			return
-		}
+	heightFloats, err := ListToFloats(heights)
+	if err != nil {
+		return 0, err
 	}
-	for i, h := range volumes {
-		switch t := h.(type) {
-		case int:
-			volumeFloats[i] = float64(t)
-		case uint:
-			volumeFloats[i] = float64(t)
-		case int8:
-			volumeFloats[i] = float64(t)
-		case uint8:
-			volumeFloats[i] = float64(t)
-		case int16:
-			volumeFloats[i] = float64(t)
-		case uint16:
-			volumeFloats[i] = float64(t)
-		case int32:
-			volumeFloats[i] = float64(t)
-		case uint32:
-			volumeFloats[i] = float64(t)
-		case int64:
-			volumeFloats[i] = float64(t)
-		case uint64:
-			volumeFloats[i] = float64(t)
-		case float32:
-			volumeFloats[i] = float64(t)
-		case float64:
-			volumeFloats[i] = t
-		default:
-			err = fmt.Errorf("The volume in position %d of the volumes can not be converted to a float64", i)
-			return
-		}
+	volumeFloats, err := ListToFloats(volumes)
+	if err != nil {
+		return 0, err
 	}
 
 	for i := range heights {
@@ -120,13 +63,49 @@ func heightToVolume(height float64, sensorOffset float64, heights []interface{},
 	for i := range heights {
 		if (height + sensorOffset) < heightFloats[i] {
 			if i == 0 {
-				break
+				return
 			}
 			ratioIncurrentHeight := (height + sensorOffset - heightFloats[i-1]) / (heightFloats[i] - heightFloats[i-1])
 			result = ratioIncurrentHeight*(volumeFloats[i]-volumeFloats[i-1]) + volumeFloats[i-1]
-			break
+			return
 		}
 		result = volumeFloats[i]
 	}
 	return
+}
+
+func ListToFloats(input []interface{}) ([]float64, error) {
+	result := make([]float64, len(input))
+
+	for i, h := range input {
+		switch t := h.(type) {
+		case int:
+			result[i] = float64(t)
+		case uint:
+			result[i] = float64(t)
+		case int8:
+			result[i] = float64(t)
+		case uint8:
+			result[i] = float64(t)
+		case int16:
+			result[i] = float64(t)
+		case uint16:
+			result[i] = float64(t)
+		case int32:
+			result[i] = float64(t)
+		case uint32:
+			result[i] = float64(t)
+		case int64:
+			result[i] = float64(t)
+		case uint64:
+			result[i] = float64(t)
+		case float32:
+			result[i] = float64(t)
+		case float64:
+			result[i] = t
+		default:
+			return []float64{}, fmt.Errorf("The value in position %d of the input can not be converted to a float64", i)
+		}
+	}
+	return result, nil
 }
---
+++ mapper/expressions_test.go
@@ -0,0 +1,148 @@
+package mapper_test
+
+import (
+	"testing"
+
+	. "github.com/munnik/gosk/mapper"
+)
+
+var (
+	heights = []interface{}{
+		1.230,
+		1.315,
+		1.390,
+		1.460,
+		1.555,
+		1.640,
+		1.705,
+		1.775,
+		1.840,
+		1.905,
+		1.975,
+		2.040,
+		2.100,
+		2.170,
+		2.240,
+		2.310,
+		2.380,
+		2.450,
+		2.520,
+		2.590,
+		2.660,
+	}
+	volumes = []interface{}{
+		4.000,
+		6.000,
+		8.000,
+		10.000,
+		12.000,
+		14.000,
+		16.000,
+		18.000,
+		20.000,
+		22.000,
+		24.000,
+		26.000,
+		28.000,
+		30.000,
+		32.000,
+		34.000,
+		36.000,
+		38.000,
+		40.000,
+		42.000,
+		44.000,
+	}
+)
+
+func TestListToFloatsEmpty(t *testing.T) {
+	result, err := ListToFloats([]interface{}{})
+	if len(result) != 0 {
+		t.Log("Length of result should be 0")
+		t.Fail()
+	}
+	if err != nil {
+		t.Log("No error should occur for empty list")
+		t.Fail()
+	}
+}
+
+func TestHeightToVolumeAlmostNoHeight(t *testing.T) {
+	vol, err := HeightToVolume(0.029, 1.2, heights, volumes)
+	if err != nil {
+		t.Log("No error expected")
+		t.Fail()
+	}
+	if vol > 4 {
+		t.Log("To much oil measured")
+		t.Fail()
+	}
+}
+
+func TestHeightToVolumeActualHeight(t *testing.T) {
+	vol, err := HeightToVolume(0.583925, 1.2, heights, volumes)
+	if err != nil {
+		t.Log("No error expected")
+		t.Fail()
+	}
+	if vol < 18 {
+		t.Logf("Not enough oil measured, %f", vol)
+		t.Fail()
+	}
+	if vol > 20 {
+		t.Logf("Too much oil measured, %f", vol)
+		t.Fail()
+	}
+}
+
+func TestCurrentToRatio(t *testing.T) {
+	var res float64
+	res = CurrentToRatio(4000)
+	if res != 0 {
+		t.Logf("Expected 0.00 but got %f", res)
+		t.Fail()
+	}
+	res = CurrentToRatio(8000)
+	if res != 0.25 {
+		t.Logf("Expected 0.25 but got %f", res)
+		t.Fail()
+	}
+	res = CurrentToRatio(12000)
+	if res != 0.50 {
+		t.Logf("Expected 0.50 but got %f", res)
+		t.Fail()
+	}
+	res = CurrentToRatio(16000)
+	if res != 0.75 {
+		t.Logf("Expected 0.75 but got %f", res)
+		t.Fail()
+	}
+	res = CurrentToRatio(20000)
+	if res != 1.00 {
+		t.Logf("Expected 1.00 but got %f", res)
+		t.Fail()
+	}
+}
+
+func TestPressureToHeight(t *testing.T) {
+	res := PressureToHeight(4810, 840)
+	if res != 0.583925 {
+		t.Logf("Expected 0.583925 but got %f", res)
+		t.Fail()
+	}
+}
+
+func TestMilliAmpereToVolume(t *testing.T) {
+	ratio := CurrentToRatio(6408 * 1.0)
+	pressure := ratio * 30000.0
+	height := PressureToHeight(pressure, 840.0)
+	res, err := HeightToVolume(height, 1.2, heights, volumes)
+	if err != nil {
+		t.Logf("Unexpected error %v", err)
+		t.Fail()
+	}
+	if res != 18 {
+		t.Logf("Expected 18 but got %f", res)
+		t.Fail()
+	}
+}

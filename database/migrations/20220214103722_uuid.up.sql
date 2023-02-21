9fcb46ea284ba97fb4da3096ed2c0f5c60b428e7
munnik <martijndemunnik@protonmail.com>
2023-02-21 15:06:37 +0100

    Create uuid-ossp extension in public schema

--- database/migrations/20220214103722_uuid.up.sql
+++ database/migrations/20220214103722_uuid.up.sql
@@ -1,7 +1,7 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" CASCADE; 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "public" CASCADE; 
 
 ALTER TABLE "raw_data"
-ADD COLUMN "uuid" UUID NOT NULL DEFAULT uuid_nil();
+ADD COLUMN "uuid" UUID NOT NULL DEFAULT "public"."uuid_nil"();
 
 ALTER TABLE "key_value_data"
-ADD COLUMN "uuid" UUID NOT NULL DEFAULT uuid_nil();
+ADD COLUMN "uuid" UUID NOT NULL DEFAULT "public"."uuid_nil"();

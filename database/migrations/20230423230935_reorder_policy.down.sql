3f0767a96d9c02d1b7148efa30815c0011877d96
munnik <martijndemunnik@protonmail.com>
2023-04-24 13:16:31 +0200

    TimescaleDB reorder policy

---
+++ database/migrations/20230423230935_reorder_policy.down.sql
@@ -0,0 +1,1 @@
+SELECT public.remove_reorder_policy('mapped_data', if_exists => true);
---
+++ database/migrations/20230423230935_reorder_policy.up.sql
@@ -0,0 +1,1 @@
+SELECT public.add_reorder_policy('mapped_data', 'mapped_data_unique_idx', if_not_exists => true);

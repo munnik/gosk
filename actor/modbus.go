98e5023937e214d423d9a7e11044e4c8158753ba
munnik <martijndemunnik@protonmail.com>
2023-06-27 21:55:36 +0200

    Cleanup an New function

--- actor/modbus.go
+++ actor/modbus.go
@@ -8,30 +8,36 @@ import (
 	"go.nanomsg.org/mangos/v3"
 )
 
-type ModbusSetter struct {
+type ModbusActor struct {
 	config config.ActorConfig
 	env    expression.ExpressionEnvironment
 }
 
-func (m *ModbusSetter) Set(subscriber mangos.Socket, publisher mangos.Socket) {
-	process(subscriber, publisher, m)
+func NewModbusActor(c config.ActorConfig) (*ModbusActor, error) {
+	return &ModbusActor{
+		config: c,
+	}, nil
+}
+
+func (a *ModbusActor) Act(subscriber mangos.Socket, publisher mangos.Socket) {
+	process(subscriber, publisher, a)
 }
 
 // DoAct creates a raw modbus message based on the set message and the setter config
-func (m *ModbusSetter) DoAct(actionRequest *message.ActionRequest) (*message.Raw, *message.ActionResponse) {
+func (a *ModbusActor) DoAct(actionRequest *message.ActionRequest) (*message.Raw, *message.ActionResponse) {
 	actionResponse := message.NewActionResponse(actionRequest)
 	vm := vm.VM{}
-	for _, actionConfig := range m.config.Actions {
+	for _, actionConfig := range a.config.Actions {
 		if actionRequest.Put.Path != actionConfig.Path {
 			continue
 		}
 
-		output, err := expression.RunExpr(vm, m.env, actionConfig)
+		output, err := expression.RunExpr(vm, a.env, actionConfig)
 		if err != nil {
 			return nil, actionResponse.WithState(message.STATE_FAILED).WithStatusCode(message.STATUS_CODE_SERVER_SIDE_ISSUE)
 		}
 		if bytes, ok := output.([]byte); ok {
-			return message.NewRaw().WithType(m.config.Protocol).WithValue(bytes),
+			return message.NewRaw().WithType(a.config.Protocol).WithValue(bytes),
 				actionResponse.WithState(message.STATE_COMPLETED).WithStatusCode(message.STATUS_CODE_SUCCESSFUL)
 		}
 		return nil, actionResponse.WithState(message.STATE_FAILED).WithStatusCode(message.STATUS_CODE_SERVER_SIDE_ISSUE)

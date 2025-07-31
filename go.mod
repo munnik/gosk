584558beeacf322b6bc59680d901e58a713e9622
Martijn de Munnik <martijndemunnik@protonmail.com>
2025-07-31 11:17:26 +0200

    Use new signalk library

--- go.mod
+++ go.mod
@@ -1,15 +1,14 @@
 module github.com/munnik/gosk
 
-go 1.24
-
-toolchain go1.24.2
+go 1.24.2
 
 require (
 	github.com/Jeffail/gabs v1.4.0
+	github.com/adrianmo/go-nmea v1.10.0
 	github.com/allegro/bigcache/v3 v3.1.0
 	github.com/brutella/can v0.0.2
 	github.com/eclipse/paho.mqtt.golang v1.5.0
-	github.com/expr-lang/expr v1.17.3
+	github.com/expr-lang/expr v1.17.5
 	github.com/fgrosse/zaptest v1.2.1
 	github.com/golang-migrate/migrate/v4 v4.18.3
 	github.com/google/uuid v1.6.0
@@ -20,7 +19,7 @@ require (
 	github.com/klauspost/compress v1.18.0
 	github.com/lxzan/gws v1.8.9
 	github.com/mitchellh/mapstructure v1.5.0
-	github.com/munnik/go-nmea v1.8.0
+	github.com/munnik/go-signalk v0.0.3
 	github.com/munnik/modbus v1.6.6
 	github.com/onsi/ginkgo/v2 v2.22.0
 	github.com/onsi/gomega v1.36.0
@@ -28,17 +27,16 @@ require (
 	github.com/spf13/cobra v1.9.1
 	github.com/spf13/viper v1.20.1
 	go.bug.st/serial v1.6.4
-	go.einride.tech/can v0.13.0
+	go.einride.tech/can v0.14.0
 	go.nanomsg.org/mangos/v3 v3.4.2
 	go.uber.org/zap v1.27.0
-	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6
+	golang.org/x/exp v0.0.0-20250718183923-645b1fa84792
 	gonum.org/v1/gonum v0.16.0
 )
 
 require (
 	github.com/BertoldVdb/go-ais v0.4.0 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
-	github.com/adrianmo/go-nmea v1.10.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/creack/goselect v0.1.3 // indirect
@@ -46,7 +44,7 @@ require (
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
-	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/pprof v0.0.0-20241206021119-61a79c692802 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
@@ -63,22 +61,22 @@ require (
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
-	github.com/prometheus/common v0.64.0 // indirect
-	github.com/prometheus/procfs v0.16.1 // indirect
-	github.com/sagikazarmark/locafero v0.9.0 // indirect
-	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/prometheus/common v0.65.0 // indirect
+	github.com/prometheus/procfs v0.17.0 // indirect
+	github.com/sagikazarmark/locafero v0.10.0 // indirect
+	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
 	github.com/spf13/afero v1.14.0 // indirect
-	github.com/spf13/cast v1.8.0 // indirect
-	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/spf13/cast v1.9.2 // indirect
+	github.com/spf13/pflag v1.0.7 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.38.0 // indirect
-	golang.org/x/net v0.40.0 // indirect
-	golang.org/x/sync v0.14.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/text v0.25.0 // indirect
-	golang.org/x/tools v0.33.0 // indirect
+	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
+	golang.org/x/tools v0.35.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- mapper/nmea0183.go
+++ mapper/nmea0183.go
@@ -5,7 +5,8 @@ import (
 	"strings"
 	"unicode"
 
-	"github.com/munnik/go-nmea"
+	"github.com/adrianmo/go-nmea"
+	"github.com/munnik/go-signalk"
 	"github.com/munnik/gosk/config"
 	"github.com/munnik/gosk/message"
 	"github.com/munnik/gosk/nanomsg"
@@ -14,19 +15,28 @@ import (
 type Nmea0183Mapper struct {
 	config   config.MapperConfig
 	protocol string
+	parser   nmea.SentenceParser
+}
+
+type customCheckCRC struct {
+	allowEmptyChecksum    bool
+	allowChecksumMismatch bool
+}
+
+func (ccc customCheckCRC) CheckCRC(sentence nmea.BaseSentence, rawFields string) error {
+	err := nmea.CheckCRC(sentence, rawFields)
+	if ccc.allowEmptyChecksum && strings.Contains(err.Error(), "nmea: sentence does not contain checksum separator") {
+		err = nil
+	}
+	if ccc.allowChecksumMismatch && strings.Contains(err.Error(), "nmea: sentence checksum mismatch") {
+		err = nil
+	}
+	return err
 }
 
 func NewNmea0183Mapper(c config.MapperConfig) (*Nmea0183Mapper, error) {
-	return &Nmea0183Mapper{config: c, protocol: config.NMEA0183Type}, nil
-}
-
-func (m *Nmea0183Mapper) Map(subscriber *nanomsg.Subscriber[message.Raw], publisher *nanomsg.Publisher[message.Mapped]) {
-	process(subscriber, publisher, m, false)
-}
-
-func (m *Nmea0183Mapper) DoMap(r *message.Raw) (*message.Mapped, error) {
 	options := []string{}
-	if optionsString, ok := m.config.ProtocolOptions[config.ProtocolOptionNmeaParse]; ok {
+	if optionsString, ok := c.ProtocolOptions[config.ProtocolOptionNmeaParse]; ok {
 		options = strings.FieldsFunc(
 			optionsString,
 			func(c rune) bool {
@@ -34,8 +44,25 @@ func (m *Nmea0183Mapper) DoMap(r *message.Raw) (*message.Mapped, error) {
 			},
 		)
 	}
+	ccc := customCheckCRC{}
+	for _, option := range options {
+		if option == "AllowEmptyChecksum" {
+			ccc.allowEmptyChecksum = true
+		}
+		if option == "AllowChecksumMismatch" {
+			ccc.allowChecksumMismatch = true
+		}
+	}
 
-	sentence, err := nmea.Parse(string(r.Value), options...)
+	return &Nmea0183Mapper{config: c, protocol: config.NMEA0183Type, parser: nmea.SentenceParser{CheckCRC: ccc.CheckCRC}}, nil
+}
+
+func (m *Nmea0183Mapper) Map(subscriber *nanomsg.Subscriber[message.Raw], publisher *nanomsg.Publisher[message.Mapped]) {
+	process(subscriber, publisher, m, false)
+}
+
+func (m *Nmea0183Mapper) DoMap(r *message.Raw) (*message.Mapped, error) {
+	sentence, err := signalk.Parse(string(r.Value), m.parser)
 	if err != nil {
 		return nil, err
 	}
@@ -44,183 +71,179 @@ func (m *Nmea0183Mapper) DoMap(r *message.Raw) (*message.Mapped, error) {
 
 	// if it is a multi fragment message return without error if it is not the last fragment
 	if aisSentence, ok := sentence.(nmea.VDMVDO); ok {
-		if numFragments, err := aisSentence.NumFragments.GetValue(); err == nil {
-			if fragmentNUmber, err := aisSentence.FragmentNumber.GetValue(); err == nil {
-				if numFragments > fragmentNUmber {
-					return result, nil
-				}
-			}
+		if aisSentence.NumFragments > aisSentence.FragmentNumber {
+			return result, nil
 		}
 	}
 
 	s := message.NewSource().WithLabel(r.Connector).WithType(m.protocol).WithUuid(r.Uuid)
 	u := message.NewUpdate().WithSource(*s).WithTimestamp(r.Timestamp)
 
-	if v, ok := sentence.(nmea.MMSI); ok {
+	if v, ok := sentence.(signalk.MMSI); ok {
 		if mmsi, err := v.GetMMSI(); err == nil {
 			result.WithContext(fmt.Sprintf("vessels.urn:mrn:imo:mmsi:%s", mmsi))
 			u.AddValue(message.NewValue().WithPath("").WithValue(message.VesselInfo{MMSI: &mmsi}))
 		}
 	}
-	if v, ok := sentence.(nmea.VesselName); ok {
+	if v, ok := sentence.(signalk.VesselName); ok {
 		if vesselName, err := v.GetVesselName(); err == nil {
 			u.AddValue(message.NewValue().WithPath("").WithValue(message.VesselInfo{Name: &vesselName}))
 		}
 	}
-	if v, ok := sentence.(nmea.VesselType); ok {
+	if v, ok := sentence.(signalk.VesselType); ok {
 		if vesselType, err := v.GetVesselType(); err == nil {
 			u.AddValue(message.NewValue().WithPath("design.aisShipType").WithValue(message.VesselType{Description: &vesselType}))
 		}
 	}
 
-	if v, ok := sentence.(nmea.DepthBelowSurface); ok {
+	if v, ok := sentence.(signalk.DepthBelowSurface); ok {
 		if depthBelowSurface, err := v.GetDepthBelowSurface(); err == nil {
 			u.AddValue(message.NewValue().WithPath("environment.depth.belowSurface").WithValue(depthBelowSurface))
 		}
 	}
-	if v, ok := sentence.(nmea.DepthBelowTransducer); ok {
+	if v, ok := sentence.(signalk.DepthBelowTransducer); ok {
 		if depthBelowTransducer, err := v.GetDepthBelowTransducer(); err == nil {
 			u.AddValue(message.NewValue().WithPath("environment.depth.belowTransducer").WithValue(depthBelowTransducer))
 		}
 	}
-	if v, ok := sentence.(nmea.FixQuality); ok {
+	if v, ok := sentence.(signalk.FixQuality); ok {
 		if fixQuality, err := v.GetFixQuality(); err == nil {
 			u.AddValue(message.NewValue().WithPath("navigation.gnss.methodQuality").WithValue(fixQuality))
 		}
 	}
-	if v, ok := sentence.(nmea.FixType); ok {
+	if v, ok := sentence.(signalk.FixType); ok {
 		if fixType, err := v.GetFixType(); err == nil {
 			u.AddValue(message.NewValue().WithPath("navigation.gnss.type").WithValue(fixType))
 		}
 	}
-	if v, ok := sentence.(nmea.RateOfTurn); ok {
+	if v, ok := sentence.(signalk.RateOfTurn); ok {
 		if rateOfTurn, err := v.GetRateOfTurn(); err == nil {
 			u.AddValue(message.NewValue().WithPath("navigation.rateOfTurn").WithValue(rateOfTurn))
 		}
 	}
-	if v, ok := sentence.(nmea.TrueCourseOverGround); ok {
+	if v, ok := sentence.(signalk.TrueCourseOverGround); ok {
 		if trueCourseOverGround, err := v.GetTrueCourseOverGround(); err == nil {
 			u.AddValue(message.NewValue().WithPath("navigation.courseOverGroundTrue").WithValue(trueCourseOverGround))
 		}
 	}
-	if v, ok := sentence.(nmea.TrueHeading); ok {
+	if v, ok := sentence.(signalk.TrueHeading); ok {
 		if trueHeading, err := v.GetTrueHeading(); err == nil {
 			u.AddValue(message.NewValue().WithPath("navigation.headingTrue").WithValue(trueHeading))
 		}
 	}
-	if v, ok := sentence.(nmea.MagneticHeading); ok {
+	if v, ok := sentence.(signalk.MagneticHeading); ok {
 		if magneticHeading, err := v.GetMagneticHeading(); err == nil {
 			u.AddValue(message.NewValue().WithPath("navigation.headingMagnetic").WithValue(magneticHeading))
 		}
 	}
-	if v, ok := sentence.(nmea.NavigationStatus); ok {
+	if v, ok := sentence.(signalk.NavigationStatus); ok {
 		if navigationStatus, err := v.GetNavigationStatus(); err == nil {
 			u.AddValue(message.NewValue().WithPath("navigation.state").WithValue(navigationStatus))
 		}
 	}
-	if v, ok := sentence.(nmea.NumberOfSatellites); ok {
+	if v, ok := sentence.(signalk.NumberOfSatellites); ok {
 		if numberOfSatellites, err := v.GetNumberOfSatellites(); err == nil {
 			u.AddValue(message.NewValue().WithPath("navigation.gnss.satellites").WithValue(numberOfSatellites))
 		}
 	}
-	if v, ok := sentence.(nmea.Position2D); ok {
+	if v, ok := sentence.(signalk.Position2D); ok {
 		if lat, lon, err := v.GetPosition2D(); err == nil {
 			// TODO: omitempty in JSON for missing altitude
 			u.AddValue(message.NewValue().WithPath("navigation.position").WithValue(message.Position{Latitude: &lat, Longitude: &lon}))
 		}
 	}
-	if v, ok := sentence.(nmea.Position3D); ok {
+	if v, ok := sentence.(signalk.Position3D); ok {
 		if lat, lon, alt, err := v.GetPosition3D(); err == nil {
 			u.AddValue(message.NewValue().WithPath("navigation.position").WithValue(message.Position{Altitude: &alt, Latitude: &lat, Longitude: &lon}))
 		}
 	}
-	if v, ok := sentence.(nmea.SpeedOverGround); ok {
+	if v, ok := sentence.(signalk.SpeedOverGround); ok {
 		if speedOverGround, err := v.GetSpeedOverGround(); err == nil {
 			u.AddValue(message.NewValue().WithPath("navigation.speedOverGround").WithValue(speedOverGround))
 		}
 	}
-	if v, ok := sentence.(nmea.SpeedThroughWater); ok {
+	if v, ok := sentence.(signalk.SpeedThroughWater); ok {
 		if speedThroughWater, err := v.GetSpeedThroughWater(); err == nil {
 			u.AddValue(message.NewValue().WithPath("navigation.speedThroughWater").WithValue(speedThroughWater))
 		}
 	}
-	if v, ok := sentence.(nmea.CallSign); ok {
+	if v, ok := sentence.(signalk.CallSign); ok {
 		if callSign, err := v.GetCallSign(); err == nil {
 			u.AddValue(message.NewValue().WithPath("communication.callsignVhf").WithValue(callSign))
 		}
 	}
-	if v, ok := sentence.(nmea.IMONumber); ok {
+	if v, ok := sentence.(signalk.IMONumber); ok {
 		if imoNumber, err := v.GetIMONumber(); err == nil {
 			u.AddValue(message.NewValue().WithPath("registrations.imo").WithValue(imoNumber))
 		}
 	}
-	if v, ok := sentence.(nmea.ENINumber); ok {
+	if v, ok := sentence.(signalk.ENINumber); ok {
 		if eniNumber, err := v.GetENINumber(); err == nil {
 			u.AddValue(message.NewValue().WithPath("registrations.other.eni.registration").WithValue(eniNumber))
 		}
 	}
-	if v, ok := sentence.(nmea.VesselLength); ok {
+	if v, ok := sentence.(signalk.VesselLength); ok {
 		if length, err := v.GetVesselLength(); err == nil {
 			// TODO: omitempty in JSON for missing hull and waterline
 			u.AddValue(message.NewValue().WithPath("design.length").WithValue(message.Length{Overall: &length}))
 		}
 	}
-	if v, ok := sentence.(nmea.VesselBeam); ok {
+	if v, ok := sentence.(signalk.VesselBeam); ok {
 		if beam, err := v.GetVesselBeam(); err == nil {
 			u.AddValue(message.NewValue().WithPath("design.beam").WithValue(beam))
 		}
 	}
-	if v, ok := sentence.(nmea.RelativeWindDirection); ok {
+	if v, ok := sentence.(signalk.RelativeWindDirection); ok {
 		if relativeWindDirection, err := v.GetRelativeWindDirection(); err == nil {
 			u.AddValue(message.NewValue().WithPath("environment.wind.angleApparent").WithValue(relativeWindDirection))
 		}
 	}
-	if v, ok := sentence.(nmea.TrueWindDirection); ok {
+	if v, ok := sentence.(signalk.TrueWindDirection); ok {
 		if trueWindDirection, err := v.GetTrueWindDirection(); err == nil {
 			u.AddValue(message.NewValue().WithPath("environment.wind.directionTrue").WithValue(trueWindDirection))
 		}
 	}
-	if v, ok := sentence.(nmea.MagneticWindDirection); ok {
+	if v, ok := sentence.(signalk.MagneticWindDirection); ok {
 		if magneticWindDirection, err := v.GetMagneticWindDirection(); err == nil {
 			u.AddValue(message.NewValue().WithPath("environment.wind.directionMagnetic").WithValue(magneticWindDirection))
 		}
 	}
-	if v, ok := sentence.(nmea.WindSpeed); ok {
+	if v, ok := sentence.(signalk.WindSpeed); ok {
 		if windSpeed, err := v.GetWindSpeed(); err == nil {
 			u.AddValue(message.NewValue().WithPath("environment.wind.speedOverGround").WithValue(windSpeed))
 		}
 	}
-	if v, ok := sentence.(nmea.OutsideTemperature); ok {
+	if v, ok := sentence.(signalk.OutsideTemperature); ok {
 		if outsideTemperature, err := v.GetOutsideTemperature(); err == nil {
 			u.AddValue(message.NewValue().WithPath("environment.outside.temperature").WithValue(outsideTemperature))
 		}
 	}
-	if v, ok := sentence.(nmea.DewPointTemperature); ok {
+	if v, ok := sentence.(signalk.DewPointTemperature); ok {
 		if dewPointTemperature, err := v.GetDewPointTemperature(); err == nil {
 			u.AddValue(message.NewValue().WithPath("environment.outside.dewPointTemperature").WithValue(dewPointTemperature))
 		}
 	}
-	if v, ok := sentence.(nmea.Humidity); ok {
+	if v, ok := sentence.(signalk.Humidity); ok {
 		if humidity, err := v.GetHumidity(); err == nil {
 			u.AddValue(message.NewValue().WithPath("environment.outside.humidity").WithValue(humidity))
 		}
 	}
-	if v, ok := sentence.(nmea.WaterTemperature); ok {
+	if v, ok := sentence.(signalk.WaterTemperature); ok {
 		if waterTemperature, err := v.GetWaterTemperature(); err == nil {
 			u.AddValue(message.NewValue().WithPath("environment.water.temperature").WithValue(waterTemperature))
 		}
 	}
-	if v, ok := sentence.(nmea.Heave); ok {
+	if v, ok := sentence.(signalk.Heave); ok {
 		if heave, err := v.GetHeave(); err == nil {
 			u.AddValue(message.NewValue().WithPath("environment.heave").WithValue(heave))
 		}
 	}
-	if v, ok := sentence.(nmea.DateTime); ok {
+	if v, ok := sentence.(signalk.DateTime); ok {
 		if dt, err := v.GetDateTime(); err == nil {
 			u.AddValue(message.NewValue().WithPath("navigation.datetime").WithValue(dt))
 		}
 	}
-	if v, ok := sentence.(nmea.RudderAngle); ok {
+	if v, ok := sentence.(signalk.RudderAngle); ok {
 		rudderAngleStarboard, errStarboard := v.GetRudderAngleStarboard()
 		rudderAnglePortSide, errPort := v.GetRudderAnglePortside()
 		if rudderAngle, err := v.GetRudderAngle(); err == nil {
@@ -233,7 +256,7 @@ func (m *Nmea0183Mapper) DoMap(r *message.Raw) (*message.Mapped, error) {
 			u.AddValue(message.NewValue().WithPath("steering.rudderAngle").WithValue(rudderAnglePortSide))
 		}
 	}
-	if v, ok := sentence.(nmea.Alarm); ok {
+	if v, ok := sentence.(signalk.Alarm); ok {
 		description, _ := v.GetDescription()
 		active, err := v.IsActive()
 		if err != nil {
